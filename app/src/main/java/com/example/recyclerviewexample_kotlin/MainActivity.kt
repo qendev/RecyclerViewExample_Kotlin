0bfcb5393e768dbbbc248c9cb8b383365af2d1ae
qendev <qendev00@gmail.com>
2020-07-11 16:55:01 +0300

    gave the recyclerview the ability to add a new item and remove item as well

--- app/src/main/java/com/example/recyclerviewexample_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexample_kotlin/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.recyclerviewexample_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private val sampleList = generateDummyList(101)
+
+    private  val adapter = SampleAdapter(sampleList)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sampleList = generateDummyList(101)
-        my_recyclerview.adapter = SampleAdapter(sampleList)
+        my_recyclerview.adapter = adapter
         my_recyclerview.layoutManager = LinearLayoutManager(this)
         my_recyclerview.setHasFixedSize(true)
     }
-    private fun generateDummyList(size:Int): List<SampleItem>{
+    private fun generateDummyList(size:Int): ArrayList<SampleItem>{
         val list = ArrayList<SampleItem>()
         for (i in 0 until size){
             val drawable = when (i % 3){
@@ -27,5 +34,27 @@ class MainActivity : AppCompatActivity() {
             list += item
         }
         return  list
+    }
+
+    fun addItem(view: View) {
+        val index:Int = Random.nextInt(8)
+
+        val newItem = SampleItem(
+            R.drawable.ic_android,
+            "New Item at Position $index",
+            "Description",
+            "Price"
+
+        )
+        sampleList.add(index, newItem)
+        adapter.notifyItemInserted(index)
+
+    }
+
+    fun removeItem(view: View) {
+        val index:Int = Random.nextInt(8)
+        sampleList.removeAt(index)
+        adapter.notifyItemRemoved(index)
+
     }
 }
